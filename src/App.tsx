16fe41567bf4aec93337635df840f8964e575ca8
Jason <llamorin.jasonbusiness@gmail.com>
2022-06-11 00:32:06 +0800

    See the magic of ci/cd

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        TESTING CI/CD
+        Hello Test
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
